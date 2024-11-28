be1883b64e3a2b5d128fadfd6ae37d6c253c1996
Evgeny Svet <kabelsea@gmail.com>
2024-11-28 16:02:00 +0700

    updated graphql fx module

--- modules/graphql/controllers.go
+++ modules/graphql/controllers.go
@@ -1,8 +1,6 @@
 package graphql
 
 import (
-	"path"
-
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/labstack/echo/v4"
@@ -11,21 +9,10 @@ import (
 
 func NewPlaygroundController(config *Config, e *echo.Echo) {
 	if p := config.Graphql.Playground; p.Enabled {
-
-		var path = path.Join(
-			"/1bb70d79-5756-4674-ae28-be0e258e664c", // TODO
-			config.Graphql.Query.Path,
-		)
-
-		e.GET(p.Path, echo.WrapHandler(playground.Handler("Playground", path)))
+		e.GET(p.Path, echo.WrapHandler(playground.Handler("Playground", config.Graphql.Query.Path)))
 	}
 }
 
 func NewQueryController(config *Config, server *handler.Server, e *echo.Echo) {
-	var path = path.Join(
-		"/:tenant",
-		config.Graphql.Query.Path,
-	)
-
-	e.Any(path, echo.WrapHandler(cache.Middleware(server)))
+	e.Any(config.Graphql.Query.Path, echo.WrapHandler(cache.Middleware(server)))
 }
