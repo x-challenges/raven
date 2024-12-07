49241a07a7957b504ca8a8a876dce83b4f6089f0
Evgeny Svet <kabelsea@gmail.com>
2024-12-07 23:04:34 +0700

    fixed fasthhp fx module, config

--- modules/fasthttp/fx.go
+++ modules/fasthttp/fx.go
@@ -3,6 +3,8 @@ package fasthttp
 import (
 	"go.uber.org/fx"
 	"go.uber.org/zap"
+
+	"github.com/x-challenges/raven/modules/config"
 )
 
 // ModuleName
@@ -11,6 +13,8 @@ const ModuleName = "fasthttp"
 // Module
 var Module = fx.Module(
 	ModuleName,
+
+	config.Inject(new(Config)),
 
 	// public usage
 	fx.Provide(
