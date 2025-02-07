5fce585e1c347685ce1e8103a40c1782c2adf823
Evgeny Svet <kabelsea@gmail.com>
2025-02-07 20:19:51 +0700

    added max idle conn options to fasthttp

--- modules/fasthttp/config.go
+++ modules/fasthttp/config.go
@@ -8,13 +8,14 @@ type Config struct {
 		Client struct {
 			// Host fasthttp client
 			Host struct {
-				MaxConnsPerHost    int           `mapstructure:"max_conns_per_host" default:"1024"`
-				MaxConnDuration    time.Duration `mapstructure:"max_conn_duration" default:"30s"`
-				MaxConnWaitTimeout time.Duration `mapstructure:"max_conn_wait_timeout" default:"10ms"`
-				ReadTimeout        time.Duration `mapstructure:"read_timeout" default:"1s"`
-				WriteTimeout       time.Duration `mapstructure:"write_timeout" default:"1s"`
-				ReadBufferSize     int           `mapstructure:"read_buffer_size" default:"1024"`
-				WriteBufferSize    int           `mapstructure:"write_buffer_size" default:"1024"`
+				MaxConnsPerHost     int           `mapstructure:"max_conns_per_host" default:"1024"`
+				MaxConnDuration     time.Duration `mapstructure:"max_conn_duration" default:"30s"`
+				MaxConnWaitTimeout  time.Duration `mapstructure:"max_conn_wait_timeout" default:"10ms"`
+				MaxIdleConnDuration time.Duration `mapstructure:"max_idle_conn_durartion" default:"10s"`
+				ReadTimeout         time.Duration `mapstructure:"read_timeout" default:"1s"`
+				WriteTimeout        time.Duration `mapstructure:"write_timeout" default:"1s"`
+				ReadBufferSize      int           `mapstructure:"read_buffer_size" default:"1024"`
+				WriteBufferSize     int           `mapstructure:"write_buffer_size" default:"1024"`
 			} `mapstructure:"host"`
 
 			// Pipeline fasthttp client
@@ -22,7 +23,7 @@ type Config struct {
 				MaxConns            int           `mapstructure:"max_conns" default:"1024"`
 				MaxPendingRequests  int           `mapstructure:"max_pending_requests" default:"1024"`
 				MaxBatchDelay       time.Duration `mapstructure:"max_batch_delay" default:"0"`
-				MaxIdleConnDuration time.Duration `mapstructure:"max_idle_conn_durartion" default:"0"`
+				MaxIdleConnDuration time.Duration `mapstructure:"max_idle_conn_durartion" default:"10s"`
 				ReadTimeout         time.Duration `mapstructure:"read_timeout" default:"1s"`
 				WriteTimeout        time.Duration `mapstructure:"write_timeout" default:"1s"`
 				ReadBufferSize      int           `mapstructure:"read_buffer_size" default:"1024"`
